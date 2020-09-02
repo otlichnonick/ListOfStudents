f35cadce21b38097376f8ab11f698353a3776688
Anton Agafonov <otlichnonick@yandex.ru>
2020-09-02 10:19:47 +0300

    Initial Commit

---
+++ ListOfStudents/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListOfStudents
+//
+//  Created by Антон on 02.09.2020.
+//  Copyright © 2020 Anton Agafonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
